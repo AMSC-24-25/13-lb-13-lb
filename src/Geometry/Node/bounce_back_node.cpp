599be5258782294afda40f2339841ec0c009381e
Luca Lamperti <luca.lamperti@mail.polimi.it>
2024-12-13 21:58:59 +0000

    add methods in bounce_back_node

--- src/Geometry/Node/bounce_back_node.cpp
+++ src/Geometry/Node/bounce_back_node.cpp
@@ -2,12 +2,28 @@
 
 template<int dim>
 void BounceBackNode<dim>::BoundaryFunction() {
+    this->u.fill(0.0); // No-slip
 }
 
 template<int dim>
 void BounceBackNode<dim>::Collide() {
+    static const std::array<int, 9> reflection_indices = {0, 3, 4, 1, 2, 7, 8, 5, 6};
+
+    for (int i = 0; i < 9; i++) {
+        this->_f[reflection_indices[i]] = this->_f_next[i];
+    }
 }
 
 template<int dim>
-void BounceBackNode<dim>::Propagate() {
+void BounceBackNode<dim>::Propagate() { //serve come parametro (Domain<dim>& domain)
+
+    auto neighbours = domain.GetNeighbours(this->_position);
+    
+    for (int direction = 0; direction < 9; ++direction) {
+        
+        if (neighbours[direction]) {
+            auto& neighbour = neighbours[direction];
+            neighbour->_f_next[direction] = this->_f[direction];
+        }
+    }
 }
